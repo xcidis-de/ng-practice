e926752e302622860c6590d96b44c5df5bc2d3d6
Devin Elder <devinelder@iceman-7.local>
2019-01-03 15:39:51 -0500

    Base commit.

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.scss', '../app.component.scss']
+})
+
+export class ServerComponent{
+    title = 'ServerComponent';
+}
